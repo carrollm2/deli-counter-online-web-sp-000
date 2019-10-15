1c5099103a8d167c8e9e79eebf519647c99d2c51
Michael Carroll <carrollm2@gmail.com>
2019-10-15 03:23:53 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,4 +12,25 @@ def line(array)
     counter += 1
   end
 
+  puts line_output
+
+end
+
+
+def take_a_number(array, name)
+  line_length = array.length
+  number = line_length + 1
+
+  puts "Welcome, #{name}. You are number #{number} in line."
+
+end
+
+
+def now_serving(array)
+
+  if !array.any?
+    return "There is nobody waiting to be served!"
+
+  puts "Currently serving #{array.shift}"  
+
 end
