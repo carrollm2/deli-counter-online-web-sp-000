2345dc78efc8844d5d6a2c2dc15b1c8cb61b0b7e
Michael Carroll <carrollm2@gmail.com>
2019-10-15 03:14:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,13 @@
-# Write your code here.
+def line(array)
+
+  if !array.any?
+    return "The line is currently empty"
+
+  line_output = "The line is currently: "
+  counter = 1
+  array.each do |name|
+    line_output += "#{counter}. #{name}"
+    counter += 1
+  end
+
+end
